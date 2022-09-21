c2a2e7596131a2a50376fb6b8e8711924b3eaa85
Jeremy Elliott <jodomoi64@yahoo.ca>
2022-09-21 15:57:37 -0600

    Gatsby tutorial 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import Layout from "../components/Layout";
 
@@ -5,6 +6,15 @@ function IndexPage() {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+
+      <StaticImage
+        src="https://ichef.bbci.co.uk/images/ic/1232x1232/p09sz4sz.jpg"
+        alt="Horrifice train crash"
+      />
+      <StaticImage
+        src="../images/egypt-train-accident.jpg"
+        alt="Another horrific train crash"
+      />
     </Layout>
   );
 }
